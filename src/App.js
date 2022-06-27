ca118e22dd00f6b44aba43947aa602bbe5c36b54
NagaveniPusuluri <93608500+NagaveniPusuluri@users.noreply.github.com>
2022-06-27 22:11:18 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <p>"I am learning React. My life is getting better."</p>
-          </div>
+    </div>
   );
 }
 
